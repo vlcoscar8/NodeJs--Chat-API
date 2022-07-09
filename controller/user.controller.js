e6ce44bc1e7c7ffb40c2caafbfa96fd40cb452f5
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-09 15:37:17 +0200

    populate chats from user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ const userDetail = async (req, res, next) => {
     try {
         const { id } = req.params;
 
-        const user = await User.findById(id);
+        const user = await User.findById(id).populate("chats");
 
         res.status(200).json(user);
     } catch (error) {

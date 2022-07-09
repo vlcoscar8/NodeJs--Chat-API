1903ebcea7081eed86ad0f51e3034d9397b1046a
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-09 12:35:06 +0200

    user find by id

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ const userDetail = async (req, res, next) => {
     try {
         const { id } = req.params;
 
-        const user = await User.findOne({ id: id });
+        const user = await User.findById(id);
 
         res.status(200).json(user);
     } catch (error) {
